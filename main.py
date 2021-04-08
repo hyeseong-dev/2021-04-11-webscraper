e5bc45e1e7fb22e2203488d839c3932c4544da25
hyeseong-lee <hyeseong43@gmail.com>
2021-04-09 06:26:09 +0900

    2021040906:26 추가 스택오버플로우 페이지네이션 작업

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from so     import get_jobs as get_so_jobs
 
 # indeed_jobs = get_indeed_jobs()
 so_jobs = get_so_jobs()
-
+print(so_jobs)
 # print(indeed_jobs)
 
 
--- so.py
+++ so.py
@@ -9,8 +9,30 @@ def get_last_page():
     result = requests.get(URL)
     soup   = BeautifulSoup(result.text, 'html.parser')
     pages = soup.find('div', {'class':'s-pagination'}).find_all('a')
-    print(pages)
+    last_page_anchor = pages[-2]
+    last_page = int(last_page_anchor.get_text().strip())
+    return last_page
+
+
+def extract_job(html):
+    title = html.find('h2', {'class':'company'})
+    print(title)
+
+
+
+
+def extract_jobs(last_page):
+    jobs = list()
+    # for page in range(last_page):
+    result = requests.get(f'{URL}&pg=0')
+    soup = BeautifulSoup(result.text, 'html.parser')
+    results = soup.find_all('div', {'class':'company-list'})
+    for result in results:
+        job = extract_job(result)
+        jobs.append(job)
+    return jobs
 
 def get_jobs():
     last_page = get_last_page()
-    return []
+    jobs = extract_jobs(last_page)
+    return jobs
